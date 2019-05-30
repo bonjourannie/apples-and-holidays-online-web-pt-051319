a03977adb1e62f8ee8061c84d0385769790c1fc4
Annie Truex <artruex@gmail.com>
2019-05-30 18:47:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,6 +61,7 @@ end
 def all_supplies_in_holidays(holiday_hash)
   
  holiday_hash.each do |season, holidays|
+   binding.pry 
     puts "#{season.to_s.capitalize!}:"
     holidays.each do |holiday, supply|
       array = holiday.to_s.split("_")
